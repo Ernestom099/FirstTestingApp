841ab6893d577adead50621b6666e406021d2a9c
Ernestom099 <82059490+Ernestom099@users.noreply.github.com>
2023-01-05 13:22:22 -0600

    init for testing

---
+++ app/src/main/java/com/example/firsttestingapp/Assertions.kt
@@ -0,0 +1,28 @@
+package com.example.firsttestingapp
+
+class Assertions {
+    private val user = User("Ernestom", 26,)
+
+    fun getLuckyNumbers(): Array<Int> {
+        return arrayOf(2, 7)
+    }
+
+    fun getName(): String {
+        return user.name
+    }
+
+    fun checkHuman(): Boolean {
+        return user.isHuman
+    }
+
+    fun checkHuman(user: User?): Boolean? {
+        return user?.isHuman
+    }
+
+    fun isAdult(user: User): Boolean {
+        if (!user.isHuman) {
+            return true
+        }
+        return user.age >= 18
+    }
+}
